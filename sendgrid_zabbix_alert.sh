843d9c768637108e692c83da2931c6230a105508
Michael Gindonis <mkgin@users.noreply.github.com>
2017-03-24 12:48:59 +0000

    Add debugging, get it working with zabbix, remove CRLF and replace with "\n"

--- sendgrid_zabbix_alert.sh
+++ sendgrid_zabbix_alert.sh
@@ -10,23 +10,47 @@ source /etc/zabbix/sendgrid_zabbix_alert.conf
 # By default alert scripts are here /usr/lib/zabbix/alertscripts
 # AlertScriptsPath can be set in zabbix_server.conf
 
+
 # Zabbix sets the following parameters when calling the alertscript
 MAILTO=$1
 MAILSUBJECT=$2
-MAILBODY=$3
+# Strip Carriage returns and linefeeds and use \n instead\n
+MAILBODY=$( echo "$3" |sed  's/\x0d/\\n/g' | tr -d "\n" )
+# Some extra curl args
+CURL_ARGS="--write-out %{http_code} --silent"
+DEBUG=0
+# debugging to write output to /tmp ... your API keys will show up in
+# in the debug info...
 
-#curl --trace-ascii curltrace --request POST \
-#curl --request POST \
-curl --write-out %{http_code} --trace-ascii curltrace --request POST \
-  --url https://api.sendgrid.com/v3/mail/send \
+if [ $DEBUG ]
+then 
+echo "$(env)" > /tmp/sendgrid_zabbix_alert_env_$(date +%s)
+echo "$MAILBODY" > /tmp/sendgrid_zabbix_alert_MAILBODY_$(date +%s)
+CURL_ARGS="--write-out %{http_code} --trace-ascii /tmp/sendgrid_zabbix_alert_curltrace_$(date +%s)"
+fi
+
+
+CURL_URL="https://api.sendgrid.com/v3/mail/send"
+CURLDATA=$( cat <<EOF
+{"personalizations": [{"to": [{"email": "${MAILTO}"}]}],
+"from": {"email": "${MAILFROMADDR}"},
+"subject": "'${MAILSUBJECT}'",
+"content": [{"type": "text/plain","value": "${MAILBODY}"}],
+"tracking_settings": {
+"click_tracking": { "enable": false},
+"open_tracking": { "enable": false}
+} }
+EOF
+)
+
+curl --request POST \
+  --url $CURL_URL \
   --header 'Authorization: Bearer '$SENDGRID_API_KEY'' \
   --header 'Content-Type: application/json' \
-  --data '{"personalizations": [{"to": [{"email": "'"$MAILTO"'"}]}], \
-"from": {"email": "'"$MAILFROMADDR"'"}, \
-"subject": "'"$MAILSUBJECT"'", \
-"content": [{"type": "text/plain","value": "'"$MAILBODY"'"}], \
-"tracking_settings": { \
-"click_tracking": { "enable": false}, \
-"open_tracking": { "enable": false} \
-} \
-}'
+  --data "'${CURLDATA}'" \
+  $CURL_ARGS
+
+echo "\nCURL EXIT $?"
+
+# We still need to check for 202 and return error otherwise.
+exit
